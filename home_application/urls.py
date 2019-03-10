6a91f7e23599bf94fd97f0a1aa692536f496b750
hanxt6 <1009107135@qq.com>
2019-03-10 18:58:27 +0800

    恢复默认访问路径

--- home_application/urls.py
+++ home_application/urls.py
@@ -13,7 +13,8 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns(
     'home_application.views',
-    (r'^$', 'helloworld'),
+    (r'^$', 'home'),
     (r'^dev-guide/$', 'dev_guide'),
     (r'^contactus/$', 'contactus'),
+    (r'^helloworld/$', 'helloworld'),
 )
